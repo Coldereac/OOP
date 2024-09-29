111cfd71ab081d57d0fa14f6ff7d102f57fca5a1
Anton Malynevskyi <malina.elfg@gmail.com>
2024-09-29 11:34:41 +0200

    4prakt vbsg

--- Prakt_4/main.cpp
+++ Prakt_4/main.cpp
@@ -1,51 +1,53 @@
 #include <iostream>
 using namespace std;
 
-class Rectangle{
-private:
+class Rectangle {
     double height;
     double width;
+
 public:
-    Rectangle(){
+    Rectangle() {
         this->height = 20;
         this->width = 30;
     }
 
-    Rectangle(double high, double width){
+    Rectangle(double high, double width) {
         this->height = high;
         this->width = width;
     }
-    void setHeight(double high){
-        if(high > 0) this->height = high;
-    }
-    void setWidth(double width){
-        if(width > 0) this->width = width;
+
+    void setHeight(double high) {
+        if (high > 0) this->height = high;
     }
 
-    double getHeight(){
+    void setWidth(double width) {
+        if (width > 0) this->width = width;
+    }
+
+    double getHeight() const {
         return this->height;
     }
 
-    double getWidth(){
+    double getWidth() const {
         return this->width;
     }
 
-    double getSquare(){
+    double getSquare() const {
         return (this->height * this->width);
     }
 
 
-    double getPerimeter(){
+    double getPerimeter() const {
         return (this->height + this->width) * 2;
     }
 
-    void display(){
-        cout << getHeight() << " " << getWidth() << " " << getPerimeter() << " " <<
-             getSquare() << endl;
+    void display() const {
+        cout << "Height: " << getHeight() << " Width:  " << getWidth() << " Perimeter:  " << getPerimeter() << " Square:  " <<
+                getSquare() << endl;
     }
 };
 
-int main(){
+int main() {
     Rectangle rect = Rectangle();
     rect.display();
     rect.setHeight(34.5);
@@ -58,12 +60,12 @@ int main(){
     arr[2] = Rectangle();
     arr[2].setHeight(34.5);
     arr[1].setWidth(67.46);
-    for(int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++) {
         arr[i].display();
     }
 
 
-    Rectangle* rectPos = new Rectangle(3.0, 6.0);
+    Rectangle *rectPos = new Rectangle(3.0, 6.0);
     rectPos->display();
     rectPos->setHeight(34.5);
     rectPos->setWidth(20.4);
